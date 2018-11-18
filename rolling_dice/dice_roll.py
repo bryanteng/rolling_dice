5953a74c896a694dce4781ec922d6c28c967f826
github username <bteng1226@gmail.com>
2018-11-18 09:45:25 -0500

    added dictionary to show roll counts for each roll

--- rolling_dice/dice_roll.py
+++ rolling_dice/dice_roll.py
@@ -3,15 +3,27 @@ import random
 def roll(num):
     rolls= []
     average = 0
-
+    rolls_dict = {}
     index = 0
+
     while index < num:
         rolls.append(random.randint(1,6))
         index+=1
     for x in rolls:
         average+= x
         print_roll(x)
+        if x in rolls_dict:
+            rolls_dict[x] = rolls_dict[x]+1
+        else:
+            rolls_dict[x] = 1
+
     print("The average roll was " +str(average/num) + " in " +str(num)+ " rolls.")
+    for key,val in rolls_dict.items():
+        print(str(key)+ " was rolled "+str(val) + " times")
+
+
+
+
 def print_roll(num):
     if num == 6:
         print("--------")

1a279f168c63cfc4edf680c5efd4977c4c1d87be
github username <bteng1226@gmail.com>
2018-11-18 09:02:14 -0500

    added dice design and printing to console

--- rolling_dice/dice_roll.py
+++ rolling_dice/dice_roll.py
@@ -7,6 +7,44 @@ def roll(num):
         rolls.append(random.randint(1,6))
         index+=1
     for x in rolls:
-        print("You rolled a %s." % x)
+        print_roll(x)
+
+def print_roll(num):
+    if num == 6:
+        print("--------")
+        print("|O     O|")
+        print("|O     O|")
+        print("|O     O|")
+        print("--------")
+    elif num == 5:
+        print("--------")
+        print("|O     O|")
+        print("|   O   |")
+        print("|O     O|")
+        print("--------")
+    elif num == 4:
+        print("--------")
+        print("|O     O|")
+        print("|       |")
+        print("|O     O|")
+        print("--------")
+    elif num == 3:
+        print("--------")
+        print("|      O|")
+        print("|   O   |")
+        print("|O      |")
+        print("--------")
+    elif num == 2:
+        print("--------")
+        print("|      O|")
+        print("|       |")
+        print("|O      |")
+        print("--------")
+    elif num == 1:
+        print("--------")
+        print("|       |")
+        print("|   O   |")
+        print("|       |")
+        print("--------")
 
 roll(6)

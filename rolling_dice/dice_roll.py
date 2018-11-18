1cf255c849a81daf81a12a07477061460b5728ce
github username <bteng1226@gmail.com>
2018-11-18 08:54:18 -0500

    string interpolation

--- rolling_dice/dice_roll.py
+++ rolling_dice/dice_roll.py
@@ -7,6 +7,6 @@ def roll(num):
         rolls.append(random.randint(1,6))
         index+=1
     for x in rolls:
-        print("You rolled a " + str(x))
+        print("You rolled a %s." % x)
 
 roll(6)
